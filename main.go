b1841233fb07bf294bf112728c48f6e9f1557f21
altynbek07 <altynbek.kazezov.97@gmail.com>
2024-11-16 15:25:14 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	transactions := []float64{}
+
+	for {
+		transaction := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
+
+	balance := calculateBalance(transactions)
+	fmt.Printf("Ваш баланс %.2f", balance)
+}
+
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Введите транзакцию (n для выхода): ")
+	fmt.Scan(&transaction)
+	return transaction
+}
+
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+
+	for _, value := range transactions {
+		balance += value
+	}
+
+	return balance
+}
